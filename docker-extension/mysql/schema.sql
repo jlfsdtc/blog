9d5773043faa525150b056e517b6dcd4c8fd8a08
jlfsdtc <1251489546@qq.com>
2018-08-11 14:08:23 +0800

    pack jar

--- docker-extension/mysql/schema.sql
+++ docker-extension/mysql/schema.sql
@@ -78,8 +78,8 @@ LOCK TABLES `t_contents` WRITE;
 
 INSERT INTO `t_contents` (`cid`, `title`, `slug`, `created`, `modified`, `content`, `author_id`, `type`, `status`, `tags`, `categories`, `hits`, `comments_num`, `allow_comment`, `allow_ping`, `allow_feed`)
 VALUES
-	(1,'about my blog','about',1487853610,1487872488,'### Hello World\r\n\r\nabout me\r\n\r\n### ...\r\n\r\n...',1,'page','publish',NULL,NULL,0,0,1,1,1),
-	(2,'Hello My Blog',NULL,1487861184,1487872798,'## Hello  World.\r\n\r\n> ...\r\n\r\n----------\r\n\r\n\r\n<!--more-->\r\n\r\n```java\r\npublic static void main(String[] args){\r\n    System.out.println(\"Hello 13 Blog.\");\r\n}\r\n```',1,'post','publish','','default',10,0,1,1,1);
+	(1,'about my blog','about',1533951117,1533951117,'### My First Website.',1,'page','publish',NULL,NULL,0,0,1,1,1),
+	(2,'Hello My Blog',NULL,1533951117,1533951117,'## Hello  World.\r\n\r\n> ...\r\n\r\n----------\r\n\r\n\r\n<!--more-->\r\n\r\n```java\r\npublic static void main(String[] args){\r\n    System.out.println(\"Hello 13 Blog.\");\r\n}\r\n```',1,'post','publish','','default',10,0,1,1,1);
 
 UNLOCK TABLES;
 
