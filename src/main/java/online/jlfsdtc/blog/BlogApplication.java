812afe15d92c5f2fb6e8a63d2545ebb8a55da544
jlfsdtc <1251489546@qq.com>
2018-09-29 07:51:06 +0800

    add Swagger2

--- src/main/java/online/jlfsdtc/blog/BlogApplication.java
+++ src/main/java/online/jlfsdtc/blog/BlogApplication.java
@@ -12,6 +12,7 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.sql.DataSource;
 import java.net.URLClassLoader;
@@ -20,6 +21,7 @@ import java.util.Arrays;
 @SpringBootApplication
 @MapperScan("online.jlfsdtc.blog.dao")
 @EnableTransactionManagement
+@EnableSwagger2
 public class BlogApplication {
     @Bean(initMethod = "init", destroyMethod = "close")
     @ConfigurationProperties(prefix = "spring.datasource")
